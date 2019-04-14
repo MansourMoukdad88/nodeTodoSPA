7a1977e567da741114551ab669a4b9d661795a21
MansourMoukdad88 <m_sy@outlook.com>
2019-04-14 16:23:22 +0300

    Define Index Route

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+let todoRoutes = require("./routes/todos");
+
 app.get("/", (req, res) => {
-  res.send({ a: "Hi There From express" });
+  res.send("Hi There root route");
 });
-app.get("/happy", (req, res) => {
-  res.send(":)");
-});
+
+app.use("/api/todos", todoRoutes);
+
 app.listen(3000, () => {
   console.log("Server Has Started on port 3000 ");
 });
