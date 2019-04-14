2d416af8563c12c402beaa9ac828aa34510a8bcb
MansourMoukdad88 <m_sy@outlook.com>
2019-04-14 15:38:04 +0300

    Initialize express with the main  routes

---
+++ todos_api/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hi There From express");
+});
+app.get("/happy", (req, res) => {
+  res.send(":)");
+});
+app.listen(3000, () => {
+  console.log("Server Has Started on port 3000 ");
+});
