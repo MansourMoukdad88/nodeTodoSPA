30f4b29f6e4156dc7870bccc01fea98903609e2c
MansourMoukdad88 <m_sy@outlook.com>
2019-04-14 23:29:21 +0300

    make it better

--- todos_api/public/main.js
+++ todos_api/public/main.js
@@ -6,7 +6,11 @@ $(document).ready(function() {
       createTodo();
     }
   });
-  $(".list").on("click", "span", function() {
+  $(".list").on("click", "li", function() {
+    updateTodo($(this));
+  });
+  $(".list").on("click", "span", function(e) {
+    e.stopPropagation();
     removeTodo($(this).parent());
   });
 });
@@ -20,6 +24,7 @@ function addTodos(todos) {
 function addTodo(todo) {
   var newTodo = $("<li class='task'>" + todo.name + "<span>X</span>" + "</li>");
   newTodo.data("id", todo._id);
+  newTodo.data("completed", todo.completed);
   if (todo.completed) {
     newTodo.addClass("done");
   }
@@ -53,4 +58,19 @@ function removeTodo(todo) {
     .catch(function(err) {
       console.log(err);
     });
+}
+
+function updateTodo(todo) {
+  var updateUrl = "/api/todos/" + todo.data("id");
+  var isDone = !todo.data("completed");
+  var updateData = { completed: isDone };
+  // console.log(updateData);
+  $.ajax({
+    method: "PUT",
+    url: updateUrl,
+    data: updateData
+  }).then(function(updatedTodo) {
+    todo.toggleClass("done");
+    todo.data("completed", isDone);
+  });
 }
