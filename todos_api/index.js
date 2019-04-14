ed9fe9f9d4f71914acbe376b312d6fddd8420ced
MansourMoukdad88 <m_sy@outlook.com>
2019-04-14 15:59:44 +0300

    Define app schema

--- todos_api/index.js
+++ todos_api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi There From express");
+  res.send({ a: "Hi There From express" });
 });
 app.get("/happy", (req, res) => {
   res.send(":)");
